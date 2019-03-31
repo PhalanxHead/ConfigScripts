67d365c0ae227442580237e93b4a7d2dd0862b8c
= <=>
2019-03-31 21:46:14 +1100

    File Sync

--- Startup/syncFiles.bat
+++ Startup/syncFiles.bat
@@ -8,11 +8,11 @@ REM Dependancies:  pypi's dirsync
 
 SET repoDir = D:\ConfigScripts\
 
-dirsync %repoDir%+"\Startup" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup" -s -o "*.ahk|syncFiles.bat"
-dirsync %repoDir%+"\nvim" "%USERPROFILE%\AppData\Local\nvim" -s -o "init.vim"
+dirsync %repoDir%\Startup "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup" -s -o "*.ahk|syncFiles.bat"
+dirsync %repoDir%\nvim "%USERPROFILE%\AppData\Local\nvim" -s -o "init.vim"
 
-dirsync "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup" %repoDir%+"\Startup" -s -o "*.ahk|syncFiles.bat"
-dirsync "%USERPROFILE%\AppData\Local\nvim" %repoDir%+"\nvim" -s -o "init.vim"
+dirsync "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup" %repoDir%\Startup -s -o "*.ahk|syncFiles.bat"
+dirsync "%USERPROFILE%\AppData\Local\nvim" %repoDir%\nvim -s -o "init.vim"
 
 cd %reopDir%
 git add .
